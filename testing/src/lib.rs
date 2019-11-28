b4dba1288aca36140ac369e4056fbbb64962f0f0
soc1c <soc1c@users.noreply.github.com>
2019-11-28 14:45:47 +0100

    testing: restore dependencies

---
+++ testing/src/lib.rs
@@ -0,0 +1,25 @@
+// Copyright 2018-2019 Parity Technologies (UK) Ltd.
+// Copyright 2019-2020 Dothereum UG (DE).
+// This file is part of Dothereum.
+
+// Dothereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Dothereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Dothereum.  If not, see <http://www.gnu.org/licenses/>.
+
+//! A set of testing utilities for Dothereum Node.
+
+#![warn(missing_docs)]
+
+pub mod client;
+pub mod genesis;
+pub mod keyring;
+
